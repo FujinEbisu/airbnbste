3e45b85ff325b6f7e2c4058b449e9fd25b0124dc
Alexandre Blaiset <alexandre.blaiset@gmail.com>
2025-05-26 17:51:59 +0200

    seed delete et rerun properly

--- app/models/friend_profile.rb
+++ app/models/friend_profile.rb
@@ -1,5 +1,5 @@
 class FriendProfile < ApplicationRecord
   belongs_to :user
-  has_many :bookings
-  has_many :reviews
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,11 @@ require 'faker'
 
 
 10.times do
-  puts "create user"
+  User.destroy_all
+  FriendProfile.destroy_all
+  Booking.destroy_all
   user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "azerty", email: Faker::Internet.email, birthday: Faker::Date.birthday(min_age: 18, max_age: 65) )
-  puts "first user"
   user2 = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "azerty", email: Faker::Internet.email, birthday: Faker::Date.birthday(min_age: 18, max_age: 65) )
-puts "2 user"
-  friend = FriendProfile.create!(username: Faker::Internet.username, interest: Faker::Lorem.paragraph, day_rate: 10.2, user: user )
- puts "friend"
+  friend = FriendProfile.create!(username: Faker::Internet.username, interest: Faker::Lorem.paragraph, day_rate: rand(0.0..15.0), user: user )
   Booking.create!(start_date: Faker::Date.in_date_period, end_date: Faker::Date.in_date_period, user: user2, friend_profile: friend  )
-puts "booking"
 end
