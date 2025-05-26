56dfdafb037283b4dc293f90f71d75b9543f5421
Alexandre Blaiset <alexandre.blaiset@gmail.com>
2025-05-26 16:30:26 +0200

    added seed

--- app/models/friend_profile.rb
+++ app/models/friend_profile.rb
@@ -1,4 +1,5 @@
 class FriendProfile < ApplicationRecord
   belongs_to :user
-  has_many :bookings, :reviews
+  has_many :bookings
+  has_many :reviews
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+
 
 10.times do
-  Friend_profile.create!(username :)
+  puts "create user"
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "azerty", email: Faker::Internet.email, birthday: Faker::Date.birthday(min_age: 18, max_age: 65) )
+  puts "first user"
+  user2 = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "azerty", email: Faker::Internet.email, birthday: Faker::Date.birthday(min_age: 18, max_age: 65) )
+puts "2 user"
+  friend = FriendProfile.create!(username: Faker::Internet.username, interest: Faker::Lorem.paragraph, day_rate: 10.2, user: user )
+ puts "friend"
+  Booking.create!(start_date: Faker::Date.in_date_period, end_date: Faker::Date.in_date_period, user: user2, friend_profile: friend  )
+puts "booking"
+end
