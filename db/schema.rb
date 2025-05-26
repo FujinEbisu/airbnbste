d9aaa77df61e5bad73f4dc9eb2cd808a3313c0a1
Alexandre Blaiset <alexandre.blaiset@gmail.com>
2025-05-26 15:18:59 +0200

    Add review model and controller but not usefull yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_26_131243) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_26_131654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_26_131243) do
     t.index ["user_id"], name: "index_friend_profiles_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "friend_profile_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_profile_id"], name: "index_reviews_on_friend_profile_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,4 +61,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_26_131243) do
   add_foreign_key "bookings", "friend_profiles"
   add_foreign_key "bookings", "users"
   add_foreign_key "friend_profiles", "users"
+  add_foreign_key "reviews", "friend_profiles"
 end
