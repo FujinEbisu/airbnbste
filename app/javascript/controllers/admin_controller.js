00fcd14909742a0aa9fd11f93455fb7cfe52628d
Alexandre Blaiset <alexandre.blaiset@gmail.com>
2025-05-30 12:13:17 +0200

    add edit/destroy to friend profile, add edit to user, add delete to booking in progress

---
+++ app/javascript/controllers/admin_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="admin"
+export default class extends Controller {
+  static targets = ["auto"]
+
+  connect() {
+
+  }
+}
